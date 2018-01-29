e976738009c1d6913b3ab20a6f9a8b69c4ec84ca
SebastianJaroszek <sebastian_jaroszek@emstories.pl>
2018-01-29 18:30:35 +0100

    add test and changes in EmployeesDao

--- src/main/java/com/github/pabloo99/jdbc/dao/EmployeesDao.java
+++ src/main/java/com/github/pabloo99/jdbc/dao/EmployeesDao.java
@@ -4,10 +4,7 @@ import com.github.pabloo99.jdbc.connection.MySqlConnector;
 import com.github.pabloo99.jdbc.entity.Employee;
 import org.apache.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -59,5 +56,50 @@ public class EmployeesDao {
         return allEmployees;
     }
 
+    public Employee findById(Integer employeeId) throws SQLException {
+
+        String query = "SELECT * FROM employees WHERE employee_id=?";
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+
+        Employee employee = null;
+
+        try {
+            connection = MySqlConnector.getMySqlConnection();
+            statement = connection.prepareStatement(query);
+            statement.setInt(1, employeeId);
+
+            rs = statement.executeQuery(query);
+
+
+            while (rs.next()) {
+                employee = new Employee();
+
+                employee.setEmployeeId(rs.getInt("employee_id"));
+                employee.setFirstName(rs.getString("first_name"));
+                employee.setLastName(rs.getString("last_name"));
+                employee.setEmail(rs.getString("email"));
+                employee.setPhoneNumber(rs.getString("phone_number"));
+                employee.setHireDate(rs.getDate("hire_date").toLocalDate());
+                employee.setJobId(rs.getString("job_id"));
+                employee.setSalary(rs.getDouble("salary"));
+                employee.setCommissionPct(rs.getDouble("commission_pct"));
+                employee.setManagerId(rs.getInt("manager_id"));
+                employee.setDepartmentId(rs.getInt("department_id"));
+
+            }
+
+        } catch (SQLException e) {
+            logger.error(e.getMessage(), e);
+        } finally {
+            rs.close();
+            statement.close();
+            connection.close();
+        }
+
+        return employee;
+    }
 
 }
--- src/test/java/com/github/pabloo99/jdbc/EmployeeDaoMySqlTest.java
+++ src/test/java/com/github/pabloo99/jdbc/EmployeeDaoMySqlTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class EmployeeDaoMySqlTest {
@@ -27,6 +28,22 @@ class EmployeeDaoMySqlTest {
         }
 
         assertTrue(result.size() > 0);
+    }
+
+    @Test
+    void shouldReturnEmployeeDataForEmployeeWithId(){
+        EmployeesDao employeesDao = new EmployeesDao();
+
+        Employee result = null;
+        try {
+            result = employeesDao.findById(100);
+        } catch (SQLException e) {
+            logger.error(e.getMessage(), e);
+        }
+
+        assertEquals(result.getFirstName(), "Steven");
+        assertEquals(result.getLastName(), "King");
+        assertEquals(result.getEmployeeId(), 100);
     }
 
 }
