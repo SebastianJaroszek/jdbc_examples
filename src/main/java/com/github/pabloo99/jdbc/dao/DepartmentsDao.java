5c40db58a9bf6a5e9b0b3ce99ff834d022b19f3d
SebastianJaroszek <sebastian_jaroszek@emstories.pl>
2018-01-31 00:19:34 +0100

    implements findById method and add test for it

--- src/main/java/com/github/pabloo99/jdbc/dao/DepartmentsDao.java
+++ src/main/java/com/github/pabloo99/jdbc/dao/DepartmentsDao.java
@@ -4,6 +4,7 @@ import com.github.pabloo99.jdbc.connection.MySqlConnector;
 import com.github.pabloo99.jdbc.entity.Employee;
 import org.apache.log4j.Logger;
 
+import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -41,5 +42,78 @@ public class DepartmentsDao {
         }
 
         return 0;
+    }
+
+    public Employee findManagerByDepartmentId(int departmentId) throws SQLException {
+
+        String query = "SELECT e.* FROM departments d JOIN employees e ON d.manager_id = e.employee_id WHERE d.department_id = ?";
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+
+        try {
+            connection = MySqlConnector.getMySqlConnection();
+            statement = connection.prepareStatement(query);
+            statement.setInt(1, departmentId);
+
+            rs = statement.executeQuery();
+
+            while (rs.next()) {
+                Employee employee = new Employee();
+
+                employee.setEmployeeId(rs.getInt("employee_id"));
+                employee.setFirstName(rs.getString("first_name"));
+                employee.setLastName(rs.getString("last_name"));
+                employee.setEmail(rs.getString("email"));
+                employee.setPhoneNumber(rs.getString("phone_number"));
+                employee.setHireDate(rs.getDate("hire_date").toLocalDate());
+                employee.setJobId(rs.getString("job_id"));
+                employee.setSalary(rs.getDouble("salary"));
+                employee.setCommissionPct(rs.getDouble("commission_pct"));
+                employee.setManagerId(rs.getInt("manager_id"));
+                employee.setDepartmentId(rs.getInt("department_id"));
+
+                return employee;
+            }
+
+        } catch (SQLException e) {
+            logger.error(e.getMessage(), e);
+        } finally {
+            rs.close();
+            statement.close();
+            connection.close();
+        }
+        return null;
+    }
+
+    public String findById(int departmentId) throws SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        String result = "";
+        String query = "SELECT * FROM departments d JOIN locations l ON d.location_id = l.location_id JOIN countries co ON l.country_id = co.country_id JOIN regions r ON co.region_id = r.region_id WHERE d.department_id = ?";
+
+        try {
+            connection = MySqlConnector.getMySqlConnection();
+            statement = connection.prepareStatement(query);
+            statement.setInt(1, departmentId);
+            rs = statement.executeQuery();
+            while (rs.next()) {
+                result = result + String.valueOf(rs.getInt("d.department_id")) + " ";
+                result = result + rs.getString("d.department_name") + " ";
+                result = result + String.valueOf(rs.getInt("l.location_id")) + " ";
+                result = result + rs.getString("l.street_address") + " ";
+                result = result + rs.getString("co.country_name") + " ";
+                result = result + rs.getString("r.region_name");
+            }
+        } catch (SQLException e) {
+            logger.error(e.getMessage(), e);
+        } finally {
+            rs.close();
+            statement.close();
+            connection.close();
+        }
+        return result;
     }
 }
--- src/test/java/com/github/pabloo99/jdbc/DepartmentsDaoMySqlTest.java
+++ src/test/java/com/github/pabloo99/jdbc/DepartmentsDaoMySqlTest.java
@@ -1,10 +1,13 @@
 package com.github.pabloo99.jdbc;
 
+import com.github.pabloo99.jdbc.dao.EmployeesDao;
+import com.github.pabloo99.jdbc.entity.Employee;
 import org.apache.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import com.github.pabloo99.jdbc.dao.DepartmentsDao;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,5 +27,34 @@ public class DepartmentsDaoMySqlTest {
 
         assertEquals(result, 6);
 
+    }
+
+    @Test
+    void shouldReturnDepartmentManager(){
+
+        DepartmentsDao departmentsDao = new DepartmentsDao();
+        Employee expected = null;
+
+        Employee result = null;
+        try {
+            result = departmentsDao.findManagerByDepartmentId(10);
+            expected = new EmployeesDao().findById(200);
+        } catch (SQLException e){
+            logger.error(e.getMessage(), e);
+        }
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void shouldReturnLocationDataForDepartment(){
+        DepartmentsDao departmentsDao = new DepartmentsDao();
+        String result = "";
+        try{
+            result = departmentsDao.findById(60);
+        } catch (SQLException e){
+            logger.error(e.getMessage(), e);
+        }
+        assertEquals(result, "60 IT 1400 2014 Jabberwocky Rd United States of America Americas");
     }
 }
